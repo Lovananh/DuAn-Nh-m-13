bfb67aec34a5b25feec746b6e312a8ca64e56a7e
Lò Văn Anh <anhlo2819@gmail.com>
2024-05-29 09:05:59 +0700

    sửa
    data

---
+++ Du An/assets/js/Affiliate_login.js
@@ -0,0 +1,30 @@
+// Khi trang đã tải xong
+document.addEventListener("DOMContentLoaded", function () {
+    // Xử lý sự kiện nhấn nút đăng nhập
+    const loginButton = document.querySelector('.login-aff a');
+    loginButton.addEventListener('click', function (event) {
+        event.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ a
+
+        // Lấy giá trị của email và mật khẩu
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+
+        // Kiểm tra xem email và mật khẩu có trống không
+        if (email === '' || password === '') {
+            alert('Vui lòng nhập email và mật khẩu.');
+            return;
+        }
+
+        // Nếu không trống, chuyển hướng đến trang index.html
+        window.location.href = '../Du An/index.html';
+    });
+
+    // Xử lý sự kiện cuộn lên đầu trang
+    const goToTopButton = document.querySelector('.go-to-top');
+    goToTopButton.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
